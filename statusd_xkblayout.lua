7abe9ea801c6d0072d300d9125912c8e0fdcc612
Eugen Zagorodniy <e.zagorodniy@gmail.com>
2012-08-01 02:26:01 +0300

    Add synchronous implementation of xkblayout

---
+++ statusd_xkblayout.lua
@@ -0,0 +1,22 @@
+-- Requires xkb-switch binary under $PATH.
+-- https://github.com/ierton/xkb-switch/
+
+local defaults = {
+  update_interval = 100,
+  xkb_switch = 'xkb-switch',
+}
+local settings = table.join(statusd.get_config('xkblayout'), defaults)
+local timer = statusd.create_timer()
+
+local function current_layout()
+  local p = io.popen(settings.xkb_switch)
+  layout = p:read()
+  p:close()
+  return layout
+end
+
+function update_xkblayout()
+  statusd.inform('xkblayout', current_layout())
+  timer:set(settings.update_interval, update_xkblayout)
+end
+update_xkblayout()
